948e5cff8fe45e0cbd20311c85c4f3ab07d5f2e4
amaan <amaan@localhost>
2025-03-11 15:49:39 +0530

    nnn

--- selenium6/src/test/java/selenium_6/GitTest.java
+++ selenium6/src/test/java/selenium_6/GitTest.java
@@ -3,7 +3,7 @@ package selenium_6;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("github");
+		System.out.print("githhub");
 		// TODO Auto-generated method stub
 
 	}
